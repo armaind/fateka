54dc8588ff7db16e04f67a7b5785bcbd22f6c117
Andi <andimilyas123@gmail.com>
2023-01-17 16:06:06 +0700

    p

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,6 +43,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'google_id' => $request->Id
         ]);
 
         event(new Registered($user));
