a1ae64ed29726d833dbcb157cfabfd2e66ba46ca
c0nyy <armaindd@gmail.com>
2023-01-18 15:23:06 +0700

    commit

--- resources/views/pages/backend/publikasi/berita/create.blade.php
+++ resources/views/pages/backend/publikasi/berita/create.blade.php
@@ -15,7 +15,7 @@
         <div class="page-title">
             <div class="card card-absolute mt-5 mt-md-4">
                 <div class="card-header bg-primary">
-                    <h5 class="text-white">Tambah Berita</h5>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                    <h5 class="text-white">Tambah Berita</h5>
                 </div>
                 <div class="card-body">
                     <p>
