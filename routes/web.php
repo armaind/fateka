52894f34c6771698b70fc7177e65573290bfca05
Andi <andimilyas123@gmail.com>
2023-01-18 17:54:12 +0700

    about

--- routes/web.php
+++ routes/web.php
@@ -38,27 +38,28 @@ Route::prefix('dashboard')
     ->group(function () {
         /* -------------------------- Dashboard Index Pages ------------------------- */
         Route::get('/', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+        
         /* ------------------------- Data Alumni ------------------------- */
         Route::get('/data-alumni', 'App\Http\Controllers\DataAlumniController@index');
         Route::get('/data-alumni/tambah-data-alumni', 'App\Http\Controllers\DataAlumniController@create');
         Route::resource('dataAlumni', 'App\Http\Controllers\DataAlumniController');
+        
         /* ------------------------- Publikasi pages ------------------------- */
         /* ------------------------- Berita ------------------------- */
         Route::get('/berita', 'App\Http\Controllers\BeritaController@index');
         Route::get('/berita/tambah-berita', 'App\Http\Controllers\BeritaController@create');
         Route::resource('berita', 'App\Http\Controllers\BeritaController');
+        
         /* ------------------------- Artikel ------------------------- */
         Route::get('/artikel', 'App\Http\Controllers\ArtikelController@index');
         Route::get('/artikel/tambah-artikel', 'App\Http\Controllers\ArtikelController@create');
         Route::resource('artikel', 'App\Http\Controllers\ArtikelController');
+        
         /* ------------------------- Lowongan Kerja Pages ------------------------ */
         Route::get('/lowongan-kerja', 'App\Http\Controllers\LowonganKerjaController@index');
         Route::get('/lowongan-kerja/tambah-lowongan-kerja', 'App\Http\Controllers\LowonganKerjaController@create');
         Route::resource('lowonganKerja', 'App\Http\Controllers\LowonganKerjaController');
-        /* ---------------------------- Whiteboard Pages ---------------------------- */
-        Route::get('/whiteboard', function () {
-            return view('pages.backend.whiteboard.index');
-        });
+        
         /* ------------------------- Virtual Meeting Section ------------------------ */
         Route::get('/virtual-meeting/skyoffice', function () {
             return view('pages.backend.virtual_meeting.skyoffice');
